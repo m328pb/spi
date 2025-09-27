d12fb56a99a66adeb01541c879be6f49db7ff0d4
zielaskowski <michal.zielaskowski@gmail.com>
2025-09-27 14:45:55 +0200

    license spelling

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,14 +4,17 @@
   - check recived text with network analyzer
  */
 #include "spi.h"
-
+#include <avr/delay.h>
 int main(){
 	SPI spi;
 	const char *txt_def = "SPI test: 1MHz";
 	const char *txt_4MHz = "SPI test: 4MHz";
+	while(1){
 	spi.init(16,PB2,true);
 	spi.send_ln((uint8_t *)txt_def,15);
 	spi.init(4);
 	spi.send_ln((uint8_t *)txt_4MHz,15);
+	_delay_ms(1000);
+	}
 	spi.off();
 }
